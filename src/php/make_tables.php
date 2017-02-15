68a9a75de59ee77740e41034d53c2c5c7d3df7ca
Aidan Doherty <aidandoherty@Aidans-MacBook-Air.local>
2017-02-15 12:50:51 +0000

    updated database info

--- src/php/make_tables.php
+++ src/php/make_tables.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mind_mapper";
+$servername = "dbhost.cs.man.ac.uk";
+$username = "mbax4ad8";
+$password = "ShitPassword64!";
+$dbname = "mbax4ad8";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);

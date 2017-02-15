4b5887cd665c41278d47b5b211f491d307ac96c2
Aidan Doherty <aidandoherty@Aidans-MacBook-Air.local>
2017-02-15 15:07:06 +0000

    new new account page

--- src/php/add_new_account.php
+++ src/php/add_new_account.php
@@ -9,8 +9,7 @@ $dbname = "mbax4ad8";
 
 $username = $_POST["username"];
 
-print "$username";
-print "$password";
+
 
 // Create connection
 $conn = mysqli_connect($servername, $db_username, $password, $dbname);
@@ -34,10 +33,20 @@ $sql = "INSERT INTO users (username, password, study_week)
 		VALUES ('$username', '$encodedPassword', '$study_week')";
 if (mysqli_query($conn, $sql)) {
     echo "Inserted account into table succesfully";
+    var url = 'php/login.php';
+	var form = $('<form action="' + url + '" method="post">' +
+	  '			<input type="text" name="username" placeholder="'+$username+'"><br>
+	  			<input type="password" name="password" placeholder="'+$_POST['password']+'"><br>' +
+	  '</form>');
+	$('body').append(form);
+	form.submit();
 } else {
     echo "Error inserting account into table: " . mysqli_error($conn);
 }
 
 mysqli_close($conn);
+
+
+
 
 ?>

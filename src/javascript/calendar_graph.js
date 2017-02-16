df50b20bb617ae495a78f91ba4042080398fbf2b
Aidan Doherty <aidandoherty@Aidans-Air.home>
2017-02-16 19:33:32 +0000

    click on activity for info

--- src/javascript/calendar_graph.js
+++ src/javascript/calendar_graph.js
@@ -3,9 +3,9 @@ var daysOffset = -1;
 
 function pointAction(event){
 	var name = event.target;
-	var firstParse = name.split("=");
-	var secondParse = firstParse[1].split(")");
-	alert(secondParse[0]);
+	//var firstParse = name.split("=");
+	//var secondParse = firstParse[1].split(")");
+	alert(event.target.nodeName);
 }
 
 var graph_data;

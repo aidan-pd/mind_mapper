5066d3d16d0691f2513c57919c71cd54d0a75012
Aidan Doherty <aidandoherty@Aidans-Air.home>
2017-02-16 18:09:04 +0000

    hour view

--- src/javascript/day_graph.js
+++ src/javascript/day_graph.js
@@ -63,7 +63,7 @@ function startSetup(){
 
 		for (var i = amountOfHoursToDisplay-1; i >= 0; i--) {
 			//alert(getDate(moment().subtract(i+daysOffset, 'days')));
-			xAxis.push(new axisPoint(getTime(moment().subtract(i+daysOffset, 'hours')), getTime(moment().subtract(i+daysOffset, 'hours'))));
+			xAxis.push(new axisPoint(getTimeLabel(moment().subtract(i+daysOffset, 'hours')), getTime(moment().subtract(i+daysOffset, 'hours'))));
 		};
 
 		/*xAxis.push(new axisPoint("1", "1"));
@@ -162,8 +162,14 @@ function handleTick(){
 }//handleTick
 
 function getTime(aTime){
-	return ""+aTime.hour()+":00";
+		return ""+aDate.date()+"-"+(aDate.month()+1)+"-"+aDate.year()+" "+aTime.hour()+":00";
+
 }
+
+function getTimeLabel(aTime){
+		return ""+aTime.hour()+":00";
+}
+
 
 function getDate(aDate){
 	return ""+aDate.date()+"-"+(aDate.month()+1)+"-"+aDate.year()+"";

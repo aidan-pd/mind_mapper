55d9255f5408cb75dff594f14ef754263dfc384e
Aidan Doherty <aidandoherty@Aidans-MacBook-Air.local>
2017-02-23 15:54:03 +0000

    google analytics

--- src/mood_entry_page.php
+++ src/mood_entry_page.php
@@ -31,6 +31,17 @@ else{
 <link rel="stylesheet" type="text/css" href="css/activity_widget.css">
 <link rel="stylesheet" type="text/css" href="css/mood_widget.css">
 
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-92514395-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
 </head>
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 

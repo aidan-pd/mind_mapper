abd2aa1ef0aef01ce3512c13ade9fb2305c01af9
Aidan Doherty <aidandoherty@Aidans-MacBook-Air.local>
2017-03-05 16:51:02 +0000

    dashboard window manager

--- src/dashboard.php
+++ src/dashboard.php
@@ -10,16 +10,9 @@
 
 	</head>
 	<body>
-		<u><p onclick="">Click Below To Dispatch Notifications</p></u>
-		<b><p onclick="dispatchNotificationSMSWeek1()">Week 1</p></b>
-		<b><p onclick="dispatchNotificationSMSWeek2()">Week 2</p></b>
-		<p>Last dispatched at:</p>
-		<p id="lastDispatchedAt"></p>
-		<p>Last successful dispatch at:</p>
-		<p id="lastSuccessfulDispatchAt"></p>
-		<p>Response:</p>
-		<img id="dispatchload" src="images/spinner.gif" height="50px">
-		<p id="response"></p>
+		<div id="windowPane"></div>
+
+
 
 
 
--- src/javascript/dashboard.js
+++ src/javascript/dashboard.js
@@ -1,3 +1,16 @@
+var dispatchNotificationsWindow = new Window("Dipatch Notifications", "dispatch_window");
+dispatchNotificationsWindow.draw();
+dispatchNotificationsWindow.append('		<u><p onclick="">Click Below To Dispatch Notifications</p></u>
+		<b><p onclick="dispatchNotificationSMSWeek1()">Week 1</p></b>
+		<b><p onclick="dispatchNotificationSMSWeek2()">Week 2</p></b>
+		<p>Last dispatched at:</p>
+		<p id="lastDispatchedAt"></p>
+		<p>Last successful dispatch at:</p>
+		<p id="lastSuccessfulDispatchAt"></p>
+		<p>Response:</p>
+		<img id="dispatchload" src="images/spinner.gif" height="50px">
+		<p id="response"></p>');
+
 
 function dispatchNotificationSMSWeek1(){
 	

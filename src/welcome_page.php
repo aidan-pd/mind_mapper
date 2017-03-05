f316bf9ba7930a818139a8e7b7546758c6a492bd
Aidan Doherty <aidandoherty@Aidans-MacBook-Air.local>
2017-03-05 16:02:20 +0000

    analytics based on week

--- src/welcome_page.php
+++ src/welcome_page.php
@@ -46,17 +46,20 @@ else{
 
 		}
 
-		$.get('php/check_study_week.php', doAlert);
+		$.get('php/check_study_week.php', function(data){
+			week = data;
+					//OVERALL DATA
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-		//OVERALL DATA
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			  ga('create', 'UA-92514395-1', 'auto');
+			  ga('send', 'pageview');
+			  ga('set', 'userId', week); // Set the user ID using signed-in user_id.
+		});
 
-		  ga('create', 'UA-92514395-1', 'auto');
-		  ga('send', 'pageview');
-		  ga('set', 'userId', week); // Set the user ID using signed-in user_id.
+
 		</script>
 
 
